8bd356bae17254e2448475ca545016a8aab1366c
matthewcho1211 <matthewcho1234@gmail.com>
2022-03-04 15:23:28 +0800

    ScoringNG

---
+++ src/main/java/com/matt/ScoringNG.java
@@ -0,0 +1,12 @@
+package com.matt;
+
+public class ScoringNG {
+    public static void main(String[] args) {
+        String[] names = {"Matt", "Rayyy", "Kevin", "Evaaa"};
+        int[] englishs = {90, 80, 70, 60};
+        int[] maths = {97, 88, 77, 65};
+        for (int i = 0; i < 4; i++) {
+            System.out.println(names[i]+"\t"+englishs[i]+"\t"+maths[i]+"\t"+(englishs[i]+maths[i])/2);
+        }
+    }
+}
