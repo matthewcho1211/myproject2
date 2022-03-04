63966152d046d674c24aa552f8fb87eb7bf3ed11
matthewcho1211 <matthewcho1234@gmail.com>
2022-03-04 16:10:47 +0800

    Scoring

---
+++ src/main/java/com/matt/Scoring.java
@@ -0,0 +1,7 @@
+package com.matt;
+
+public class Scoring {
+    public static void main(String[] args) {
+        Student matt = new Student("Matt", 70, 88);
+    }
+}
---
+++ src/main/java/com/matt/Student.java
@@ -0,0 +1,12 @@
+package com.matt;
+
+public class Student {
+    String name;
+    int english;
+    int math;
+    public Student(String name, int english, int math){
+        this.name = name;
+        this.english = english;
+        this.math = math;
+    }
+}
