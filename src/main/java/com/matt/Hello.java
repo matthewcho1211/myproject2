42218261c751e34e0d824ce797c7941a94064956
matthewcho1211 <matthewcho1234@gmail.com>
2022-05-13 16:26:32 +0800

    Parking

--- src/main/java/com/matt/Hello.java
+++ src/main/java/com/matt/Hello.java
@@ -1,7 +1,13 @@
 package com.matt;
 
+
+import com.matt.score.Student;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("安安你好");
+        Student s1 = new Student("matt", 1, 1);
     }
+
+
 }
---
+++ src/main/java/com/matt/parking/Running.java
@@ -0,0 +1,42 @@
+package com.matt.parking;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Running {
+    public static void main(String[] args) {
+        String id = "ABC-1234";
+        String enterTime = "11:00";
+        String exitTime = "14:17";
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        Date d = null;
+        try {
+            d = sdf.parse(enterTime);
+        } catch (Exception e) {
+            System.out.println("wrong format");
+        }
+        System.out.println(d);
+        System.out.println(d.getTime());
+
+        try {
+            Date d2 = sdf.parse(exitTime);
+            System.out.println(d2);
+            System.out.println(d2.getTime());
+            long ms = d2.getTime() - d.getTime();
+            int minutes = (int)ms/(1000*60);
+            System.out.println("total minutes: " + minutes);
+            int hours = (minutes)/60;
+            System.out.println("hours: "+hours);
+            int diff = minutes%60;
+            System.out.println("minutes: "+diff);
+            int fee = hours*30;
+            if(diff >= 15 && diff <= 30){
+                fee += 15;
+            }
+            System.out.println(fee);
+
+        } catch (Exception e) {
+            System.out.println("exit wrong format");
+        }
+
+    }
+}
