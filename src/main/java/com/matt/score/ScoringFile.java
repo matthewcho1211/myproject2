0ecd030c180a5d8e69814bd168c94e2ab2ac385e
matthewcho1211 <matthewcho1234@gmail.com>
2022-06-17 16:31:00 +0800

    BufferedReader used

--- src/main/java/com/matt/score/ScoringFile.java
+++ src/main/java/com/matt/score/ScoringFile.java
@@ -1,38 +1,45 @@
 package com.matt.score;
 
-import javax.imageio.IIOException;
+
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
+
 import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
 
 public class ScoringFile {
     public static void main(String[] args) {
-        String lines = null;
-        ArrayList<String[]>list = new ArrayList<>();
 
         try {
             FileReader fileReader = new FileReader("scores.txt");
-//            BufferedReader bufferedReader = new BufferedReader(fileReader);
-//            while ((lines = bufferedReader.readLine()) != null){
-//                String[] line = lines.split(",");
-//                list.add(line);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String lines = bufferedReader.readLine();
+            while (lines != null){
+                String[] token = lines.split(",");
+                if(token.length == 4){
+                    String name = token[0];
+                    int english = Integer.parseInt(token[1]);
+                    int math = Integer.parseInt(token[2]);
+                    int thesis = Integer.parseInt(token[3]);
+                    GraduateStudent graduateStudent = new GraduateStudent(name, english, math, thesis);
+                    graduateStudent.print();
+                }else{
+                    String name = token[0];
+                    int english = Integer.parseInt(token[1]);
+                    int math = Integer.parseInt(token[2]);
+                    Student stu = new Student(name, english, math);
+                    stu.print();
+                }
+                lines = bufferedReader.readLine();
+            }
+//            int data = fileReader.read();
+//            while (data != -1){
+//                System.out.print((char)data);
+//                data = fileReader.read();
 //            }
-            int data = fileReader.read();
-            System.out.println(data);
-            data = fileReader.read();
-            System.out.println(data);
-
-        }catch (FileNotFoundException e){
-            System.out.println("檔案讀取失敗");
-        }catch (IOException e){
+        }catch (Exception e){
             System.out.println("檔案讀取失敗");
         }
 
-//        for(String[] x : list){
-//            System.out.println(x[0]+"\t"+x[1]+"\t"+x[2]);
-//        }
+
 
     }
 }
